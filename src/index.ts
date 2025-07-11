148d1f3a51fda5fc460d88b04378b230a5248443
Sayon Batabyal <batabyalsayon@gmail.com>
2025-07-11 15:16:36 +0530

    Fix: listen on 0.0.0.0 and use Railway port

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import app from "./app";
 
 
-app.listen(3000, () => {
-    console.log('Application Running on http://localhost:3000');
+const PORT = parseInt(process.env.PORT || "3000", 10);
+
+app.listen(PORT, '0.0.0.0' , () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
